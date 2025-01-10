31580acd2f0a496c64949cc7f1a55dd02ee016f6
Gabelloide <olivier.gabelle1@gmail.com>
2025-01-10 16:24:23 +0100

    ComfyUI Cuda12.4

---
+++ comfyui-cu124/runner-scripts/download_models.sh
@@ -0,0 +1,12 @@
+echo "########################################"
+echo "[INFO] Downloading Models..."
+echo "########################################"
+
+# Models
+cd /root/ComfyUI/models
+aria2c \
+  --input-file=/runner-scripts/download-models.txt \
+  --allow-overwrite=false \
+  --auto-file-renaming=false \
+  --continue=true \
+  --max-connection-per-server=5
